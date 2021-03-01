478ff1e4f792cd9fbed0c7fcbb29e63c145bd506
menna-lotfy <78800380+menna-lotfy@users.noreply.github.com>
2021-03-01 22:36:40 +0200

    Update Q4.sql

--- Q4.sql
+++ Q4.sql
@@ -1,6 +1,6 @@
 SELECT Customer.FirstName, Customer.LastName, Invoice.Total
 FROM Customer
 JOIN Invoice
-WHERE Customer.CustomerId = Invoice.CustomerId
+ON Customer.CustomerId = Invoice.CustomerId
 ORDER BY Invoice.Total DESC
 LIMIT 10;
